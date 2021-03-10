8f08e85d315d8321ebb5458422f5de6030014076
khan <khan@kh.octa.com>
2021-03-09 20:51:58 -0600

    Javascript Prototype Pollution

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ### All the POCs and garbage code goes here.
 
 * Parameter Pollution - `routes/prototypepollution`
+
+    Request: `http://localhost:3000/proto?attr=__proto__&value={"admin":"true"}`
--- routes/prototypepollution/index.js
+++ routes/prototypepollution/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Something that works
+// Rquest - http://localhost:3000/proto?attr=__proto__&value={"admin":"true"}
 router.get('/', function(req, res, next) {
   const obj = Object.create(null)
   obj[req.query.attr] = JSON.parse(req.query.value)
   console.log(obj.__proto__)
   res.send("Yeah whatever")
 });
+
+// Something that might work?
+router.get('/2', function(req, res, next) {
+    const obj = Object.assign({}, JSON.parse(req.query.value))
+    res.send("Yeah whatever")
+  });
 
 module.exports = router;
